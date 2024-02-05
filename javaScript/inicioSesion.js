89d3a9700d6b861fd784ff7cff29f3b1d4df3d79
inesrivilla <alumno.91172@iesazarquiel.es>
2024-02-05 18:56:05 +0100

    inicioSesion visual terminada

--- javaScript/inicioSesion.js
+++ javaScript/inicioSesion.js
@@ -1,11 +1,17 @@
-
+let hayError = false
 
 function enviarFormulario() {
     // Obtener los valores de los inputs
+
     const email = document.getElementById('email').value;
-    const password = document.getElementById('password').checked;
-  
+    const password = document.getElementById('password').value;
+    const storage= localStorage.setItem('email', email);
     // Realizar alguna acción con los valores (en este caso, solo los mostramos en la consola)
     console.log('Email:', email);
     console.log('Contraseña:', password);
+
+    if(email === "" ){
+        hayError = true;
+
+    }
   }
