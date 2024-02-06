e2f0d77a725a5335acd98a96951ee3ec61b6b44c
inesrivilla <alumno.91172@iesazarquiel.es>
2024-02-06 12:20:50 +0100

    funcion inicioSesion js

--- javaScript/inicioSesion.js
+++ javaScript/inicioSesion.js
@@ -1,17 +1,54 @@
-let hayError = false
+function showPassword() {
+    const ojo = document.getElementById('ojo');
+    const ojoCerrado = document.getElementById('ojoCerrado');
+    var passwordInput = document.getElementById("password");
+
+    if (ojo.style.display !== 'none') {
+        passwordInput.type = "text";
+        ojo.style.display = 'none';
+        ojoCerrado.style.display = 'inline';
+    } else {
+        passwordInput.type = "password";
+        ojo.style.display = 'inline';
+        ojoCerrado.style.display = 'none';
+    }
+}
+
+
+
 
 function enviarFormulario() {
-    // Obtener los valores de los inputs
+    const inicioForm = [];
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const storage= localStorage.setItem('email', email);
-    // Realizar alguna acción con los valores (en este caso, solo los mostramos en la consola)
-    console.log('Email:', email);
-    console.log('Contraseña:', password);
+    const check = document.getElementById('casillaVerificacion').checked;
 
-    if(email === "" ){
-        hayError = true;
 
+    const formData = {
+        "email": email,
+        "password": password,
+        "check": check
+    };
+    inicioForm.push(formData);
+   
+    if( validarDatos(email, password)){
+        localStorage.setItem('email', email);
+        window.location.href = 'inicio.html';
     }
   }
+
+
+  function validarDatos(email, password) {
+    if (email === '' ||password === '' ) {
+        document.getElementById("emailError").style.display = "block";
+        document.getElementById("passwordError").style.display = "block";
+    
+        return false; 
+    }else{
+        emailError.style.display = "none";
+        passwordError.style.display = "none";
+        return true; 
+    }
+    
+}
