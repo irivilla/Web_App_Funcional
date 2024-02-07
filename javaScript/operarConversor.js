e57f2f8f73e2910f5faa27398fa7039761f07832
Mike0895 <michael.morocho05@gmail.com>
2024-02-07 13:21:44 +0100

    Add pages and styles

---
+++ javaScript/operarConversor.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fromCurrencySelect = document.getElementById('fromCurrency');
+    const toCurrencySelect = document.getElementById('toCurrency');
+    const amountInput = document.getElementById('amountInput');
+    const convertButton = document.getElementById('convertButton');
+    const resultElement = document.getElementById('result');
+    const closeButton = document.getElementById('closeButton');
+
+    const convertCurrency = () => {
+        const amount = amountInput.value;
+        const fromCurrency = fromCurrencySelect.value;
+        const toCurrency = toCurrencySelect.value;
+        const host = 'api.frankfurter.app';
+        const url = `https://${host}/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`;
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then((data) => {
+                const conversionResult = `${amount} ${fromCurrency} = ${data.rates[toCurrency]} ${toCurrency}`;
+                resultElement.textContent = conversionResult;
+            })
+            .catch(error => {
+                console.error('Error al obtener datos de la API:', error);
+                resultElement.textContent = 'Asegurate de seleccionar diferentes divisas';
+            });
+    };
+
+    convertButton.addEventListener('click', convertCurrency);
+
+    amountInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            convertCurrency();
+        }
+    });
+
+});
