114aaf3bcbd21ffb16eb7c5dcf7fd5f3331b02e6
PaulaAcostaEstigarribia <paulaacosta6554@gmail.com>
2024-02-15 01:51:53 +0100

    Introducir html y js

--- javaScript/inicio.js
+++ javaScript/inicio.js
@@ -1,5 +1,56 @@
+const imagenes = document.querySelectorAll(".imagen_tarjeta");
+const ids = [document.getElementById('item1'),document.getElementById('item2'),document.getElementById('item3'),document.getElementById('item4')];
+const btns = [document.getElementById('btn1'),document.getElementById('btn2'),document.getElementById('btn3'),document.getElementById('btn4')];
+let index=0;
+let position=0;
+let scaled = [false,true,true,true];
 
-const email= localStorage.getItem('email');
+var correo = localStorage.getItem('email');
+//correo = null;
+if(!(correo==null)){
+    document.getElementById('nombre').innerText=correo;
+}
 
-const nombre = document.querySelector("h1");
-nombre.innerText = email;
+function Move(pos){
+    index = pos;
+    let positions = [-280*index-2,-330*index-2,-398*index-2];
+    for(let k=0;k<positions.length;k++){
+        const slider_content = document.querySelector(".slider_content");
+        const cssObj = window.getComputedStyle(slider_content);
+        let tamano_pantalla = cssObj.getPropertyValue("--tamaño_pantalla");
+        if (tamano_pantalla==k){
+            positionstr = positions[k].toString()+"px";
+        }
+    }
+    for (let i=0;i<imagenes.length;i++){
+        imagenes[i].style.left = positionstr;
+    }
+    for(let j=0;j<btns.length;j++){
+        if (pos==j){
+            btns[j].style.setProperty('background-color','#ffd00d');
+            scaled[j]=false;
+        }else{
+            btns[j].style.setProperty('background-color','#b7b7b7');
+            scaled[j]=true;
+        }
+    }
+    for(let j=0;j<btns.length;j++){
+        if(scaled[j]){
+            imagenes[j].style.setProperty('transform','scale(0.5)');
+        }else{
+            imagenes[j].style.setProperty('transform','scale(1)');
+        }
+    }
+}
+
+function left(){
+    if (index==0){
+        Move(3);
+    }else{
+        Move((index-1));
+    }
+}
+
+function right(){
+    Move((index+1)%4);
+}
